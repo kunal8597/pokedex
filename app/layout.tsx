7591a0b04b20f397d2365fb4c51640d9978b9ccf
Kunal Mathur <93191673+kunal8597@users.noreply.github.com>
2024-01-15 17:43:57 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,8 @@ import "./globals.css";
 const dmSans = DM_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Anime Vault",
-  description: "Your favorite anime, all in one place.",
+  title: "Pokédex",
+  description: "",
 };
 
 export default function RootLayout({
